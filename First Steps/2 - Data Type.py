282f22c90da520a099269760ec95ca55415393e9
M-viz <86562227+M-viz@users.noreply.github.com>
2022-03-21 19:01:41 +0100

    Add files via upload

---
+++ First Steps/2 - Data Type.py
@@ -0,0 +1,30 @@
+#References:
+# Data Analysis with Python for Excel Users - Full Course 
+# Course developed by Frank Andrade. 
+# Reachable at ( https://www.youtube.com/watch?v=WcDaZ67TVRo&t=1s )
+
+########################### Data Type #########################################
+
+#Just an introduction with example on the most common data types
+# 
+# type() method returns class type of the argument(object) passed as parameter. 
+# type() function is mostly used for debugging purposes.
+# Quote from (https://www.geeksforgeeks.org/python-type-function/)
+
+type(1)
+# Out[10]: int
+type(2.4)
+# Out[11]: float
+type(True)
+#Out[2]: bool
+
+type("A")
+#Out[3]: str
+
+# An important feature of the string data type is that 
+# it has Methods, we can apply different methods to strings 
+
+"a".capitalize() # We can avoid to use the print function, it works as well. 
+#Out[5]: 'A'
+
+
