f5b03bdebd6608b535acfbd62a5524876c938d10
M-viz <86562227+M-viz@users.noreply.github.com>
2023-04-26 22:51:02 +0200

    Add files via upload

---
+++ Py4e/Chapter 3 - Conditionals/ex_03_01.py
@@ -0,0 +1,8 @@
+hours=float(input("hours:"))
+rate=float(input("rate:"))
+extra_hours=hours-40
+if extra_hours >0:
+    pay=((40*rate)+(extra_hours*(rate*1.5)))
+else:
+    pay=hours*rate
+print("pay: ",pay)
