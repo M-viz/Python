9161d12f62f75c56aef97765ac53c3c9f4b7597d
Matteo Vizza <matteovizza@Matteos-MacBook-Air.local>
2024-04-09 19:36:17 +0200

    elementi presenti, da implementare la formattazione

--- Freecodecamp - scientific computing with python /Projects/1_Arithmetic_Formatter.py
+++ Freecodecamp - scientific computing with python /Projects/1_Arithmetic_Formatter.py
@@ -1,19 +1,34 @@
 def arithmetic_arranger(problems, show_answers=False):
     pt1=[]
     pt2=[]
-    sign=[]
+    signs=[]
+    answers=[]
+    underscores=[]
     for i in range(len(problems)): 
         if problems[i].find('+')!=-1:
-            sign.append('+')
+            signs.append('+')
             pt1.append(problems[i][:problems[i].find('+')-1])
             pt2.append(problems[i][problems[i].find('+')+2:])
+            sum_pt1=int(pt1[i])
+            sum_pt2=int(pt2[i])
+            final_sum=sum_pt1+sum_pt2
+            answers.append(final_sum)
+            if pt1[i] > pt2[i]:
+                underscores.append(len(pt1[i])+2)
+            else:
+                underscores.append(len(pt2[i])+2)
         elif problems[i].find('-')!=-1:
-            sign.append('-')
+            signs.append('-')
             pt1.append(problems[i][:problems[i].find('-')-1])
             pt2.append(problems[i][problems[i].find('-')+2:])
-        else:
-            return '"Error: Operator must be '+' or '-'."' 
-    return pt1,pt2,sign
-x=arithmetic_arranger("32 + 698", "3801 - 2", "45 + 43", "123 + 49")
-print(x)
+            diff_pt1=int(pt1[i])
+            diff_pt2=int(pt2[i])
+            final_diff=diff_pt1-diff_pt2
+            answers.append(final_diff)
+            if pt1[i] > pt2[i]:
+                underscores.append(len(pt1[i])+2)
+            else:
+                underscores.append(len(pt2[i])+2)
+    #return pt1,pt2,signs,answers,underscores
+    return f"{' '.join(pt1)}\n{' '.join(signs+pt2)} {' '.join(pt2)}\n{'_' * max(underscores)}"
 print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
