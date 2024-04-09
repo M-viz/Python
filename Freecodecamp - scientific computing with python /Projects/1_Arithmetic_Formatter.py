573679eff2c0f71ca785dbc6edddfe7f62c3fa75
Matteo Vizza <matteovizza@Matteos-MacBook-Air.local>
2024-04-09 17:36:11 +0200

    version 1

--- Freecodecamp - scientific computing with python /Projects/1_Arithmetic_Formatter.py
+++ Freecodecamp - scientific computing with python /Projects/1_Arithmetic_Formatter.py
@@ -1,1 +1,19 @@
-
+def arithmetic_arranger(problems, show_answers=False):
+    pt1=[]
+    pt2=[]
+    sign=[]
+    for i in range(len(problems)): 
+        if problems[i].find('+')!=-1:
+            sign.append('+')
+            pt1.append(problems[i][:problems[i].find('+')-1])
+            pt2.append(problems[i][problems[i].find('+')+2:])
+        elif problems[i].find('-')!=-1:
+            sign.append('-')
+            pt1.append(problems[i][:problems[i].find('-')-1])
+            pt2.append(problems[i][problems[i].find('-')+2:])
+        else:
+            return '"Error: Operator must be '+' or '-'."' 
+    return pt1,pt2,sign
+x=arithmetic_arranger("32 + 698", "3801 - 2", "45 + 43", "123 + 49")
+print(x)
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
