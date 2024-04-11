cd2ddc047fa9e71be99da2d88fc7190510dad2fd
Matteo Vizza <matteovizza@Matteos-MacBook-Air.local>
2024-04-11 16:05:14 +0200

    4 test su 12

--- Freecodecamp - scientific computing with python /Projects/2_Time_Calculator.py
+++ Freecodecamp - scientific computing with python /Projects/2_Time_Calculator.py
@@ -19,9 +19,13 @@ def add_time(start, duration):
     if (int(start_minutes)+int(duration_minutes))-60 > 0: 
         plus_hour_minutes_flag = 1
         am_pm_minutes =((int(start_minutes)+int(duration_minutes))-60)
+        if len(str(am_pm_minutes)) == 1:
+            am_pm_minutes= '0'+str(am_pm_minutes)
     else:
         plus_hour_minutes_flag = 0
         am_pm_minutes =(int(start_minutes)+int(duration_minutes))
+        if len(str(am_pm_minutes)) ==1 :
+            am_pm_minutes= '0'+str(am_pm_minutes)
     
     #Hours
     if (int(start_hours)+int(duration_hours))-12 > 0: 
@@ -30,7 +34,7 @@ def add_time(start, duration):
         am_pm_hours =(int(start_hours)+int(duration_hours))+plus_hour_minutes_flag
     
     #AM PM
-    hours_trivial_sum=int(start_hours)+int(duration_hours)
+    hours_trivial_sum=int(start_hours)+int(duration_hours)+plus_hour_minutes_flag
     am_pm_flag=int(hours_trivial_sum/12)
     if am_pm_original_flag =='AM':
         if am_pm_flag == 0 or am_pm_flag%2 ==0:
@@ -43,14 +47,31 @@ def add_time(start, duration):
         else:
             am_pm_final = 'AM'
         
+    #Next Day
+    next_day_check = 0 
+    next_day_trivial_sum=0
+    if am_pm_original_flag == 'AM':
+        next_day_trivial_sum = int(start_hours)+int(duration_hours)+plus_hour_minutes_flag
+    else:
+        next_day_trivial_sum = (int(start_hours)+12)+int(duration_hours)+plus_hour_minutes_flag
+    next_day_flag=0
+    if int(next_day_trivial_sum/24) <1:
+        next_day_flag=0
+        next_day_text=0
+    if int(next_day_trivial_sum/24) ==1:
+        next_day_flag=1
+        next_day_text ='(next day)'
+    if int(next_day_trivial_sum/24) >1:
+        next_day_flag = int(next_day_trivial_sum/24)
+        next_day_text = f'{next_day_flag} days later'
     
 
-
-    new_totalhour=int(start_hours)+int(duration_hours)
-    new_totalminutes=int(start_minutes)+int(duration_minutes)    
+    if next_day_flag == 0:
+        new_time=f"{am_pm_hours}:{am_pm_minutes} {am_pm_final}"
+    else:
+        new_time=f"{am_pm_hours}:{am_pm_minutes} {am_pm_final} {next_day_text}"
     
-    new_time=f"{am_pm_hours}:{am_pm_minutes} {am_pm_final}"
 
     return new_time
-    #return new_time,am_pm_original_flag,start_fmt,start_hours,start_minutes,duration_hours,duration_minutes,am_pm_minutes,am_pm_hours,am_pm_flag
-print(add_time('12:30 PM', '9:32'))
+    #return new_time,am_pm_original_flag,start_fmt,start_hours,start_minutes,duration_hours,duration_minutes,am_pm_minutes,am_pm_hours,am_pm_flag,next_day_trivial_sum,next_day_flag
+print((add_time('2:59 AM', '24:00')))
